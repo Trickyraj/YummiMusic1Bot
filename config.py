994ee2ecc2144fe15d46aee85e75511c942a8e05
Trickyraj <137736232+Trickyraj@users.noreply.github.com>
2023-09-01 11:17:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://t.me/ZaynWorldChatting",
+    "https://github.com/Trickyraj/YummiMusic1Bot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
